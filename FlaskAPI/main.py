fbf05dda8bb1f1136e14d209d0f4be9580b96f98
unknown <Gustavo.silva123913@gmail.com>
2026-04-28 23:12:35 -0300

    Aprendendo sobre como utiliziar APIs publicas

--- FlaskAPI/main.py
+++ FlaskAPI/main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import urllib.request, json
 app = Flask(__name__)
 app.json.sort_keys = False
 #apartir daqui eu dei inicio a outro curso, pois não achei mais videos
@@ -34,6 +35,13 @@ def pagina2():
         if request.form.get("Aluno") and request.form.get("Nota"):
             registros.append({'Aluno': request.form.get("Aluno"), 'Nota': request.form.get("Nota")})
     return render_template("pag2.html", registros = registros)
+@app.route("/filmes", methods = ['GET'])
+def filmes():
+    url = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=b1fca6c44a64f9b3b62030634509a9bf'
+    reposta = urllib.request.urlopen(url)
+    dados = reposta.read()
+    jsondata = json.loads(dados)
+    return render_template("filmes.html", filmes = jsondata['results'])
 
 #Aqui e onde eu "ativo" o site
 if __name__ == '__main__':
