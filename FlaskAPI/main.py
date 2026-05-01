fa38cd12badeb8d97026d24debcc7224429f09bb
unknown <Gustavo.silva123913@gmail.com>
2026-05-01 00:30:10 -0300

    Adicionando novas paginas na sesao de filmes

--- FlaskAPI/main.py
+++ FlaskAPI/main.py
@@ -1,23 +1,13 @@
 from flask import Flask, render_template, request
 import urllib.request, json
+from dotenv import load_dotenv
+import os
 app = Flask(__name__)
 app.json.sort_keys = False
-#apartir daqui eu dei inicio a outro curso, pois não achei mais videos
-#do antigo criador
+load_dotenv()
+API_KEY = os.getenv("api_key")
 
-#Rotas
-#Rotas são aonde acontece a criação de cada pagina do site
 
-#Metodo GET, ele serve para buscar dados em algum banco de dados, listas e etc...
-#ele indica ao decoretor para aceitar apenas buscas de dados, nao permitindo
-#outras coisas como a inserção de dados
-
-#Função make_response, serve para darmos uma reposta mais modificada ao cliente
-#ele permite a personalização da resposta
-#Função Jsonify, ele transforma toda list, dict em dados em formato json para
-#se comunicar
-
-#Metodo POST, usado para inserir algum valor a sua tabela
 registros = []
 frutas = ["banana", "maça", "Melancia"]
 @app.route("/", methods = ['GET'])
@@ -35,13 +25,28 @@ def pagina2():
         if request.form.get("Aluno") and request.form.get("Nota"):
             registros.append({'Aluno': request.form.get("Aluno"), 'Nota': request.form.get("Nota")})
     return render_template("pag2.html", registros = registros)
-@app.route("/filmes", methods = ['GET'])
-def filmes():
-    url = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=b1fca6c44a64f9b3b62030634509a9bf'
-    reposta = urllib.request.urlopen(url)
-    dados = reposta.read()
-    jsondata = json.loads(dados)
+@app.route("/filmes_populares", methods = ['GET'])
+def filmes_populares():
+    url = f'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key={API_KEY}'
+    api_reposta = urllib.request.urlopen(url)
+    dados_brutos = api_reposta.read()
+    jsondata = json.loads(dados_brutos)
     return render_template("filmes.html", filmes = jsondata['results'])
+@app.route("/filmes_infantis", methods = ['GET'])
+def filmes_infantis():
+    url = f'https://api.themoviedb.org/3/discover/movie?with_genres=16&certification_country=US&certification.lte=PG&sort_by=popularity.desc&api_key={API_KEY}'
+    api_resposta = urllib.request.urlopen(url)
+    dados_brutos = api_resposta.read()
+    jsondata = json.loads(dados_brutos)
+    return render_template("filmes.html", filmes = jsondata['results'])
+@app.route("/filmes_em_cartaz", methods = ['GET'])
+def filmes_em_cartaz():
+    url = f'https://api.themoviedb.org/3/movie/now_playing?api_key={API_KEY}'
+    api_resposta = urllib.request.urlopen(url)
+    dados_brutos = api_resposta.read()
+    jsondata = json.loads(dados_brutos)
+    return render_template("filmes.html", filmes = jsondata['results'])
+
 
 #Aqui e onde eu "ativo" o site
 if __name__ == '__main__':
