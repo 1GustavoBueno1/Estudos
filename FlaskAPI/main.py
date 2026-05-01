8f90234ffc8d8743be1cf31a579a81bcdefefc54
unknown <Gustavo.silva123913@gmail.com>
2026-05-01 18:53:14 -0300

    Apredendendo e aplicando o conceito de rotas dinaminas

--- FlaskAPI/main.py
+++ FlaskAPI/main.py
@@ -25,29 +25,17 @@ def pagina2():
         if request.form.get("Aluno") and request.form.get("Nota"):
             registros.append({'Aluno': request.form.get("Aluno"), 'Nota': request.form.get("Nota")})
     return render_template("pag2.html", registros = registros)
-@app.route("/filmes_populares", methods = ['GET'])
-def filmes_populares():
-    url = f'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key={API_KEY}'
+@app.route("/filmes/<propriedade>", methods = ['GET'])
+def filmes(propriedade):
+    if propriedade == 'populares':
+        url = f'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key={API_KEY}'
+    elif propriedade == 'infantil':
+        url = f'https://api.themoviedb.org/3/discover/movie?with_genres=16&certification_country=US&certification.lte=PG&sort_by=popularity.desc&api_key={API_KEY}'
+    elif propriedade == 'filmes_em_cartaz':
+        url = f'https://api.themoviedb.org/3/movie/now_playing?api_key={API_KEY}'
     api_reposta = urllib.request.urlopen(url)
     dados_brutos = api_reposta.read()
     jsondata = json.loads(dados_brutos)
     return render_template("filmes.html", filmes = jsondata['results'])
-@app.route("/filmes_infantis", methods = ['GET'])
-def filmes_infantis():
-    url = f'https://api.themoviedb.org/3/discover/movie?with_genres=16&certification_country=US&certification.lte=PG&sort_by=popularity.desc&api_key={API_KEY}'
-    api_resposta = urllib.request.urlopen(url)
-    dados_brutos = api_resposta.read()
-    jsondata = json.loads(dados_brutos)
-    return render_template("filmes.html", filmes = jsondata['results'])
-@app.route("/filmes_em_cartaz", methods = ['GET'])
-def filmes_em_cartaz():
-    url = f'https://api.themoviedb.org/3/movie/now_playing?api_key={API_KEY}'
-    api_resposta = urllib.request.urlopen(url)
-    dados_brutos = api_resposta.read()
-    jsondata = json.loads(dados_brutos)
-    return render_template("filmes.html", filmes = jsondata['results'])
-
-
-#Aqui e onde eu "ativo" o site
 if __name__ == '__main__':
     app.run(debug = True)
