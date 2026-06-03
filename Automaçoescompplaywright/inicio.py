8422d8b17fdf6e47d07446f12c9621c3ab64ad3b
unknown <Gustavo.silva123913@gmail.com>
2026-06-03 17:21:03 -0300

    Navegando entre paginas

--- Automaçoescompplaywright/inicio.py
+++ Automaçoescompplaywright/inicio.py
@@ -4,5 +4,6 @@ import time
 with sync_playwright() as play:
     browser = play.chromium.launch(headless = False)
     pagina = browser.new_page()
+    pagina.goto("https://conecte.celesc.com.br/autenticacao/login")
     time.sleep(5)
     #browser.close()
