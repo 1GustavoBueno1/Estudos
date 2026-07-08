a92849e332f46800edb8e7093065d06049ccdab3
gustavo <gustavo.silva123913@gmail.com>
2026-07-08 18:12:58 -0300

    Aprendendo sobre async e await

---
+++ javascrip/3.js
@@ -0,0 +1,28 @@
+function esperar() {
+    return new Promise((resolve) => {
+        setTimeout(() => resolve("pronto"), 1000);
+    });
+};
+
+
+async function rodar() {
+    const resposta = await esperar();
+    console.log(resposta)
+};
+
+async function buscar() {
+    try {
+    const resposta = await fetch("https://jsonplaceholder.typicode.com/todos/99999999");
+    if (!resposta.ok) {
+        throw new Error(`Erro ${resposta.status}`)
+    }
+    const dados = await resposta.json();
+    return dados
+} catch(Erro){
+    console.log(`fodeu deu esse erro: ${Erro}`)
+};
+};
+
+
+
+console.log(await buscar())
