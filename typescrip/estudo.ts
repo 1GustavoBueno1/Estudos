c0692bfb2f1182b768a75172ef9d311fbf22542d
gustavo <gustavo.silva123913@gmail.com>
2026-07-15 22:17:35 -0300

    Aprendendo typescript

---
+++ typescrip/estudo.ts
@@ -0,0 +1,14 @@
+const email = "gustavo@gmail.com";
+const idade = 17;
+email = 16
+
+
+function saudar(nome: string): string {
+    return `Ola ${nome}`
+}
+saudar("gustavo")
+
+function dobrar(numero: number): number {
+    return numero * 2
+}
+dobrar("10")
