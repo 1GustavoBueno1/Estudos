c2293ca2072cf31749b43d2bd10232b6b6cb2c08
gustavo <gustavo.silva123913@gmail.com>
2026-09-14 18:02:20 -0300

    4 bug resolvido, ao deletar alguns items o valor da soma total permanecia igual

--- api_chalenge4/services/Cart.js
+++ api_chalenge4/services/Cart.js
@@ -12,15 +12,18 @@ class Cart {
   removeItemsById(id) {
     for (let i = this.items.length - 1; i >= 0; i--) {
       if (this.items[i].id === id) {
+        this.total -= this.items[i].price
         this.items.splice(i, 1)
       }
     }
   }
 
   duplicate() {
-    const deep_copy = structuredClone(this.items)
+    const deep_copy_item = structuredClone(this.items)
+    const deep_copy_total = structuredClone(this.total)
     const newcart = new Cart()
-    newcart.items = deep_copy
+    newcart.items = deep_copy_item
+    newcart.total = deep_copy_total
     return newcart
   }
 }
